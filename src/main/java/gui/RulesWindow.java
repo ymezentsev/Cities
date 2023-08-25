3c615d23953b32a798b44820db31ac329b8654fe
angrybobr <120685363+angrybobr@users.noreply.github.com>
2023-08-25 11:33:27 +0300

    Window contains rules for cities game

---
+++ src/main/java/gui/RulesWindow.java
@@ -0,0 +1,40 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+//має викликатися при натисканні на кнопку "Правила Гри"
+public class RulesWindow {
+    String btnOk = "Ok";                //ті всі значення залежать від локалі, поки так
+    String txtRules = "Cities Game Rules:\n" +
+            "\n" +
+            "1. The objective of the game is to name a city that starts " +
+            "with the last letter of the previously named city.\n" +
+            "\n" +
+            "2. The cities named must be real cities that exist in the UK.\n" +
+            "\n" +
+            "3. Each city can only be used once in a game.\n" +
+            "\n" +
+            "4. Game ends when computer run out of the words, or player enters \"I give up.\" instead of city name\n";
+    String titleRules = "How to play \"Cities\" game";
+    public void showModalDialog(JFrame parentFrame) {
+        JDialog dialog = new JDialog(parentFrame, titleRules, true);
+        dialog.setSize(600, 250);
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+        JTextArea textArea = new JTextArea(txtRules);
+        textArea.setEditable(false);
+
+        JButton closeButton = new JButton(btnOk);
+        closeButton.addActionListener(e -> dialog.dispose());
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(new JScrollPane(textArea), BorderLayout.NORTH);
+        panel.add(closeButton, BorderLayout.SOUTH);
+
+        dialog.getContentPane().add(panel);
+        dialog.setLocationRelativeTo(parentFrame);
+        dialog.setVisible(true);
+    }
+}
+
