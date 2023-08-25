ffa0da4a41409313d543175412c3cb8b9d97afe8
OlliSensible <127432509+OlliSensible@users.noreply.github.com>
2023-08-25 13:56:27 +0200

    check what the error is

---
+++ src/main/java/guiproperties/ConsolePanel.java
@@ -0,0 +1,23 @@
+package guiproperties;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+class ConsolePanel extends JPanel {
+    private ResourceBundle bundle;
+
+    ConsolePanel(Locale locale) {
+        bundle = ResourceBundle.getBundle("guiproperties.messages", locale);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JTextField inputField = new JTextField();
+        JButton makeMoveButton = new JButton(bundle.getString("makeMove"));
+
+        add(Box.createVerticalStrut(80));
+        add(inputField);
+        add(makeMoveButton);
+        add(Box.createVerticalStrut(100));
+    }
+}
