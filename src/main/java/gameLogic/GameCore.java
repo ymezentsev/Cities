ce58de966357a7b6b7401c189d9d175391322c64
Yurii Mezentsev <yvmezentsev@gmail.com>
2023-08-26 00:36:17 +0300

    Main game loop

--- src/main/java/gameLogic/GameCore.java
+++ src/main/java/gameLogic/GameCore.java
@@ -8,10 +8,11 @@ import java.util.*;
 public class GameCore {
 
     //значення залежні від локалі
-    private String fileName = "src/main/resources/UA_cities.tx";
+    private String fileName = "src/main/resources/UA_cities.txt";
     private String exitWord = "здаюсь";
     private String errorFirstLetter = "Ви ввели місто з неправильною першою буквою";
     private String errorCity = "Такого міста не існує. Введіть інше місто";
+    private String repeatCity = "Таке місто вже було. Введіть інше місто";
     //
 
 
@@ -34,17 +35,26 @@ public class GameCore {
 
     private void gameLoop() {
         String lastComputerCity = null;
+        List<String> usedCities = new ArrayList<>();
+
 
         //Scanner для перевірки, згодом замінити на результат роботи методу обробки кнопки "Зробити хід"
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             //Scanner для перевірки, згодом замінити на результат роботи методу обробки кнопки "Зробити хід"
-            String inputCity = scanner.nextLine();
+            String inputCity = scanner.nextLine().toLowerCase().trim();
 
             if (inputCity.equalsIgnoreCase(exitWord)) {
                 //замінити на вивід вікна закінчення гри, програш юзера
                 System.exit(0);
+            }
+
+            //перевірки
+            if(usedCities.contains(inputCity)){
+                //замінити на вивід модального вікна з помилкою, текст підтягувати в залежності від мови
+                System.out.println(repeatCity);
+                continue;
             }
 
             if (lastComputerCity != null) {
@@ -60,37 +70,44 @@ public class GameCore {
                 System.out.println(errorCity);
                 continue;
             }
+
+            //додати перевірки міста, введеного користувачем
+            //у разі неправильного ввода виводити модальне вікно з помилкою, текст підтягувати в залежності від мови
+
             countUserStep++;
-            cities.remove(Character.toUpperCase(inputCity.charAt(0)) + inputCity.substring(1).toLowerCase());
+            usedCities.add(inputCity);
+            cities.remove(inputCity);
 
 
             char lastChar = inputCity.charAt(inputCity.length() - 1);
             lastComputerCity = getRandomCity(lastChar);
 
             //замінити на вивід міста в основне вікно
-            System.out.println(lastComputerCity);
+            System.out.println(Character.toUpperCase(lastComputerCity.charAt(0))
+                    + lastComputerCity.substring(1).toLowerCase());
 
             countComputerStep++;
+            usedCities.add(lastComputerCity);
             cities.remove(lastComputerCity);
         }
     }
 
     private String getRandomCity(char firstChar) {
-        List<String> properCities = new ArrayList<>();
+        List<String> suitableCities = new ArrayList<>();
         for (String city : cities) {
-            if (Character.toLowerCase(city.charAt(0)) == firstChar) {
-                properCities.add(city);
+            if (city.charAt(0) == firstChar){
+                suitableCities.add(city);
             }
         }
 
-        if (properCities.size() == 0) {
+        if (suitableCities.size() == 0) {
             //замінити на вивід вікна закінчення гри, програш ПК
             System.out.println("You win");
             System.exit(0);
         }
 
         Random random = new Random();
-        int index = random.nextInt(properCities.size());
-        return properCities.get(index);
+        int index = random.nextInt(suitableCities.size());
+        return suitableCities.get(index);
     }
 }
--- src/main/java/gameLogic/Validator.java
+++ src/main/java/gameLogic/Validator.java
@@ -18,3 +18,4 @@ public class Validator {
         return Character.toLowerCase(lastComputerCityChar) == Character.toLowerCase(firstInputChar);
     }
 }
+
