953db9304963a526ca8ba43becfa9890bc77d291
angrybobr <120685363+angrybobr@users.noreply.github.com>
2023-08-28 13:43:55 +0300

    add .properties files values

--- src/main/java/gui/RulesWindow.java
+++ src/main/java/gui/RulesWindow.java
@@ -2,27 +2,23 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ResourceBundle;
 
 //Contains rules for "Cities" game.
 public class RulesWindow {
-    String btnOk = "Ok";                //ті всі значення залежать від локалі, поки так
-    String txtRules = "Cities Game Rules:\n" +
-            "\n" +
-            "1. The objective of the game is to name a city that starts " +
-            "with the last letter of the previously named city.\n" +
-            "\n" +
-            "2. The cities named must be real cities that exist in the UK.\n" +
-            "\n" +
-            "3. Each city can only be used once in a game.\n" +
-            "\n" +
-            "4. Game ends when computer run out of the words, or player enters \"I give up.\" instead of city name\n";
-    String titleRules = "How to play \"Cities\" game";
-    public RulesWindow(JFrame parentFrame){
-        showModalDialog(parentFrame);
+    private final String btnOk;
+    private final String txtRules;
+    private final String titleRules;
+    public RulesWindow(ResourceBundle resourceBundle){
+        this.txtRules = resourceBundle.getString("txtRules");
+        this.btnOk = resourceBundle.getString("btnOk");
+        this.titleRules = resourceBundle.getString("titleRules");
+
     }
-    private void showModalDialog(JFrame parentFrame) {
+
+    public void showModalDialog(JFrame parentFrame) {
         JDialog dialog = new JDialog(parentFrame, titleRules, true);
-        dialog.setSize(600, 250);
+        dialog.setSize(500, 300);
         dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 
         JTextArea textArea = new JTextArea(txtRules);
@@ -30,7 +26,6 @@ public class RulesWindow {
 
         JButton closeButton = new JButton(btnOk);
         closeButton.addActionListener(e -> dialog.dispose());
-
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         buttonPanel.add(closeButton);
 
